4948c619271855727030bf5ab9744ce667ced93e
James <jamesward0001@gmail.com>
2024-03-03 02:12:50 +0000

    Add comments to explain debugging

--- Rust-Embedded/embassy_initial_test/src/main.rs
+++ Rust-Embedded/embassy_initial_test/src/main.rs
@@ -20,9 +20,8 @@ use embassy_initial_test::embassy_led_matrix::{self, LedMatrix};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p: embassy_stm32::Peripherals = embassy_stm32::init(Default::default());
-    let mut led: Output<'_, embassy_stm32::peripherals::PB7> = Output::new(p.PB7, Level::High, Speed::Low);
 
-
+    //This now works to store the pins in an array
     let raw_pins: [AnyPin; 8] = [
         p.PA0.into(),
         p.PA1.into(),
@@ -34,6 +33,17 @@ async fn main(_spawner: Spawner) {
         p.PA7.into(),
     ];
 
+    //This fails to compile with the below error
+    /*
+    error[E0508]: cannot move out of type `[AnyPin; 8]`, a non-copy array
+  --> src\main.rs:38:21
+   |
+   |         Output::new(raw_pins[0], Level::Low, Speed::Low),
+   |                     ^^^^^^^^^^^
+   |                     |
+   |                     cannot move out of here
+   |                     move occurs because `raw_pins[_]` has type `AnyPin`, which does not implement the `Copy` trait
+    */
     let output_pins: [Output<'_, AnyPin>; 8] = [
         Output::new(raw_pins[0], Level::Low, Speed::Low),
         Output::new(raw_pins[1], Level::Low, Speed::Low),
@@ -45,6 +55,8 @@ async fn main(_spawner: Spawner) {
         Output::new(raw_pins[7], Level::Low, Speed::Low),
     ];
 
+    //prepackaged sample code
+    let mut led: Output<'_, embassy_stm32::peripherals::PB7> = Output::new(p.PB7, Level::High, Speed::Low);
     loop {
         info!("Hello, World!");
         led.set_high();
