8475efe9bd587d57981da2f22f368101c9472889
James Ward <jamesward0001@gmail.com>
2024-10-06 23:26:18 +0100

    feat(usb_serial): Move usb serial to its own function

--- Rust-Embedded/usb_serial/src/main.rs
+++ Rust-Embedded/usb_serial/src/main.rs
@@ -14,7 +14,7 @@ use embassy_time::{Duration, Timer};
 use embassy_futures::join::join;
 use fmt::info;
 
-use embassy_stm32::{bind_interrupts, peripherals, Config, usb};
+use embassy_stm32::{bind_interrupts, peripherals, Config, usb, Peripherals};
 use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
 use embassy_usb::Builder;
 use embassy_usb::driver::EndpointError;
@@ -31,72 +31,13 @@ async fn main(_spawner: Spawner) {
 
     let config: Config = generate_blackpill_clock_config();
     
-    let p = embassy_stm32::init(config);
+    let mut p: Peripherals = embassy_stm32::init(config);
     // Spawn the LED task.
     // Degrading the pin to AnyPin allows us to pass it to the task.
     // Usually Embassy pins are singletons but we can degrade them to AnyPin to allow for more generic handling
     _spawner.spawn(led_task(p.PC13.degrade())).unwrap();
 
-    // Create the driver, from the HAL.
-    let mut ep_out_buffer = [0u8; 256];
-    let mut config = embassy_stm32::usb::Config::default();
-
-    config.vbus_detection = false;
-
-    let usb_driver = Driver::new_fs(p.USB_OTG_FS, Irqs, p.PA12, p.PA11, &mut ep_out_buffer, config);
-
-    // Create embassy-usb Config
-    let mut usb_config = embassy_usb::Config::new(0xc0de, 0xcafe);
-    usb_config.manufacturer = Some("Embassy");
-    usb_config.product = Some("USB-serial example");
-    usb_config.serial_number = Some("12345678");
-
-    // Required for windows compatibility.
-    usb_config.device_class = 0xEF;
-    usb_config.device_sub_class = 0x02;
-    usb_config.device_protocol = 0x01;
-    usb_config.composite_with_iads = true;
-
-    // Create embassy-usb DeviceBuilder using the driver and config.
-    // It needs some buffers for building the descriptors.
-    let mut usb_config_descriptor = [0; 256];
-    let mut usb_bos_descriptor = [0; 256];
-    let mut usb_control_buf = [0; 64];
-
-    let mut usb_state = State::new();
-
-    let mut usb_builder = Builder::new(
-        usb_driver,
-        usb_config,
-        &mut usb_config_descriptor,
-        &mut usb_bos_descriptor,
-        &mut [], // no msos descriptors
-        &mut usb_control_buf,
-    );
-
-    // Create classes on the builder.
-    let mut class = CdcAcmClass::new(&mut usb_builder, &mut usb_state, 64);
-
-    // Build the builder.
-    let mut usb = usb_builder.build();
-
-    // Run the USB device.
-    let usb_fut = usb.run();
-
-    // Make a future that waits for the USB to connect, then echos data.
-    let echo_fut = async {
-        loop {
-            class.wait_connection().await;
-            info!("Connected");
-            let _ = echo(&mut class).await;
-            info!("Disconnected");
-        }
-    };
-
-    // Wait for BOTH the usb and echo futures to complete before continuing.
-    // This is a simple way to run both tasks in parallel and wait for both to finish.
-    join(usb_fut, echo_fut).await;
-
+    config_and_start_usb_serial(&mut p.USB_OTG_FS, &mut p.PA12, &mut p.PA11).await.unwrap();
 }
 
 #[embassy_executor::task]
@@ -159,4 +100,76 @@ fn generate_blackpill_clock_config() -> Config {
         config.rcc.mux.clk48sel = mux::Clk48sel::PLL1_Q; // 48Mhz
     }
     return config;
+}
+
+async fn config_and_start_usb_serial<Dp, Dm>(
+    usb_otg_fs: &mut peripherals::USB_OTG_FS,
+    dp: &mut Dp,
+    dm: &mut Dm,
+) -> Result<(), ()>
+where
+    // This allows the function to accept any pin that implements the usb::DpPin and usb::DmPin traits.
+    Dp: usb::DpPin<peripherals::USB_OTG_FS>,
+    Dm: usb::DmPin<peripherals::USB_OTG_FS>,
+    {
+
+    let mut ep_out_buffer = [0u8; 256];
+    let mut config = usb::Config::default();
+
+    config.vbus_detection = false;
+
+    let usb_driver = Driver::new_fs(usb_otg_fs, Irqs, dp, dm, &mut ep_out_buffer, config);
+
+    // Create embassy-usb Config
+    let mut usb_config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    usb_config.manufacturer = Some("usb-serial example");
+    usb_config.product = Some("USB-serial example");
+    usb_config.serial_number = Some("12345678");
+
+    // Required for windows compatibility.
+    usb_config.device_class = 0xEF;
+    usb_config.device_sub_class = 0x02;
+    usb_config.device_protocol = 0x01;
+    usb_config.composite_with_iads = true;
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut usb_config_descriptor = [0; 256];
+    let mut usb_bos_descriptor = [0; 256];
+    let mut usb_control_buf = [0; 64];
+
+    let mut usb_state = State::new();
+
+    let mut usb_builder = Builder::new(
+        usb_driver,
+        usb_config,
+        &mut usb_config_descriptor,
+        &mut usb_bos_descriptor,
+        &mut [], // no msos descriptors
+        &mut usb_control_buf,
+    );
+
+    // Create classes on the builder.
+    let mut class = CdcAcmClass::new(&mut usb_builder, &mut usb_state, 64);
+
+    // Build the builder.
+    let mut usb = usb_builder.build();
+
+    // Run the USB device.
+    let usb_fut = usb.run();
+
+    // Make a future that waits for the USB to connect, then echos data.
+    let echo_fut = async {
+        loop {
+            class.wait_connection().await;
+            info!("Connected");
+            let _ = echo(&mut class).await;
+            info!("Disconnected");
+        }
+    };
+
+    // Wait for BOTH the usb and echo futures to complete before continuing.
+    // This is a simple way to run both tasks in parallel and wait for both to finish.
+    join(usb_fut, echo_fut).await;
+    return Ok(());
 }
