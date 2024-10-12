d870128318dbc7f17e9ec39aac8f600085a25e8e
James Ward <jamesward0001@gmail.com>
2024-10-12 17:05:25 +0100

    wip(postcard-rpc-sandbox): add schema and host_client

---
+++ Rust-Embedded/postcard-rpc-sandbox/host/src/client.rs
@@ -0,0 +1,112 @@
+use std::convert::Infallible;
+use postcard_rpc::{
+    host_client::{HostClient, HostErr},
+    schema::{WireError, ERROR_PATH},
+};
+use schema::{BadPositionError, GetUniqueIdEndpoint, PingEndpoint, SetAllLedEndpoint, SetSingleLedEndpoint, SingleLed};
+
+pub struct WorkbookClient {
+    pub client: HostClient<WireError>,
+}
+
+#[derive(Debug)]
+pub enum WorkbookError<E> {
+    Comms(HostErr<WireError>),
+    Endpoint(E),
+}
+
+impl<E> From<HostErr<WireError>> for WorkbookError<E> {
+    fn from(value: HostErr<WireError>) -> Self {
+        Self::Comms(value)
+    }
+}
+
+trait FlattenErr {
+    type Good;
+    type Bad;
+    fn flatten(self) -> Result<Self::Good, WorkbookError<Self::Bad>>;
+}
+
+impl<T, E> FlattenErr for Result<T, E> {
+    type Good = T;
+    type Bad = E;
+    fn flatten(self) -> Result<Self::Good, WorkbookError<Self::Bad>> {
+        self.map_err(WorkbookError::Endpoint)
+    }
+}
+
+// ---
+
+impl WorkbookClient {
+    pub fn new() -> Self {
+        let client =
+            HostClient::new_raw_nusb(|d| d.product_string() == Some("ov-twin"), ERROR_PATH, 8);
+        Self { client }
+    }
+
+    pub async fn wait_closed(&self) {
+        self.client.wait_closed().await;
+    }
+
+    pub async fn ping(&self, id: u32) -> Result<u32, WorkbookError<Infallible>> {
+        let val = self.client.send_resp::<PingEndpoint>(&id).await?;
+        Ok(val)
+    }
+
+    pub async fn get_id(&self) -> Result<u64, WorkbookError<Infallible>> {
+        let id = self.client.send_resp::<GetUniqueIdEndpoint>(&()).await?;
+        Ok(id)
+    }
+
+    pub async fn set_led_single(
+        &self,
+        position: u32,
+        brightness: u8,
+    ) -> Result<(), WorkbookError<BadPositionError>> {
+        self.client
+            .send_resp::<SetSingleLedEndpoint>(&SingleLed {
+                position,
+                brightness,
+            })
+            .await?
+            .flatten()
+    }
+
+    pub async fn set_all_brightness_single(
+        &self,
+        brightness: u8,
+    ) -> Result<(), WorkbookError<Infallible>> {
+        self.client
+            .send_resp::<SetAllLedEndpoint>(&[brightness; 24])
+            .await?;
+        Ok(())
+    }
+/*
+    pub async fn start_accelerometer(
+        &self,
+        interval_ms: u32,
+        range: AccelRange,
+    ) -> Result<(), WorkbookError<Infallible>> {
+        self.client
+            .send_resp::<StartAccelerationEndpoint>(&StartAccel { interval_ms, range })
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn stop_accelerometer(&self) -> Result<bool, WorkbookError<Infallible>> {
+        let res = self
+            .client
+            .send_resp::<StopAccelerationEndpoint>(&())
+            .await?;
+
+        Ok(res)
+    }
+    */
+}
+
+impl Default for WorkbookClient {
+    fn default() -> Self {
+        Self::new()
+    }
+}
